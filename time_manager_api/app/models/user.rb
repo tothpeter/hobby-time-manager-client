84955ef33a66bc30c9b4a7a1acc44ed977517236
Peter Toth <tothpeter08@gmail.com>
2018-02-25 18:55:34 +0100

    Add tiddle gem and downgrade devise a bit

--- time_manager_api/app/models/user.rb
+++ time_manager_api/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :confirmable, :token_authenticatable
 end
