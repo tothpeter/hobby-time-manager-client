32374a4e75862327d1605d79c0f1fc8fd873d19d
Peter Toth <tothpeter08@gmail.com>
2018-02-25 19:26:36 +0100

    Config generators

--- time_manager_api/config/application.rb
+++ time_manager_api/config/application.rb
@@ -29,5 +29,10 @@ module TimeManagerApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
   end
 end
