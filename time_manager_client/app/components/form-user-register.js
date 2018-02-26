4f32e6b1391b201d6799eff3245f764875142bb7
Peter Toth <tothpeter08@gmail.com>
2018-02-26 16:37:28 +0100

    Use better naming and hack temporarily legit forms' external error source bug

--- time_manager_client/app/components/form-user-register.js
+++ time_manager_client/app/components/form-user-register.js
@@ -1,8 +1,19 @@
 import Component from '@ember/component';
 import Object from '@ember/object';
 import { w } from '@ember/string';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+  externalErrors: computed('model.errors.[]', function() {
+    let clone = {}
+
+    this.get('model.errors').forEach(function(error){
+      clone[error.attribute] = error.message;
+    });
+
+    return clone;
+  }),
+
   rules: {
     sharedValidations: {
       required: w('email password username')
