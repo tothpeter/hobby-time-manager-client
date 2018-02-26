e279a17b14540a01334a91fbe8770a6ce137795d
Peter Toth <tothpeter08@gmail.com>
2018-02-26 04:01:53 +0100

    Add user create endpoint to the API

---
+++ time_manager_api/app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created
+    else
+      respond_with_errors @user
+    end
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:data).require(:attributes).permit(:email, :password, :username, :first_name, :last_name)
+    end
+end
---
+++ time_manager_api/spec/controllers/users_controller_spec.rb
@@ -0,0 +1,28 @@
+describe UsersController, type: :controller do
+
+  describe "POST #create" do
+    it "creates a new user and returns it" do
+      request_params = {
+        data: {
+          attributes: {
+            email: 'test@test.com',
+            password: 123456,
+            username: 'Username',
+            first_name: 'First name',
+            last_name: 'Last name'
+          },
+
+          type:"users"
+        }
+      }
+
+      expect {
+        post :create, params: request_params
+      }.to change(User, :count).by(1)
+
+      expect(json_response[:data][:email]).to eq(request_params[:data][:email])
+      expect(response).to have_http_status(:created)
+    end
+  end
+
+end
