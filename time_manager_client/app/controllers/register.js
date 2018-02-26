f5fea97c59581fcd543751353f3d309a9476377a
Peter Toth <tothpeter08@gmail.com>
2018-02-26 16:54:24 +0100

    Login user after registration on the frontend

--- time_manager_client/app/controllers/register.js
+++ time_manager_client/app/controllers/register.js
@@ -1,9 +1,22 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+  session: service(),
+
   actions: {
     register() {
-      this.get('model').save();
+      let controller = this;
+
+      this.get('model')
+        .save()
+        .then(() => {
+          let { email, password } = controller.model.getProperties('email', 'password');
+
+          controller.get('session').authenticate('authenticator:devise', email, password).then(function() {
+            controller.transitionToRoute('application');
+          });
+        });
     }
   }
 });
