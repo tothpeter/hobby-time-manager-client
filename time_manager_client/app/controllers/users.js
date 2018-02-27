4d5cbd6248223186df360c09ccba9aa46e25bf9b
Peter Toth <tothpeter08@gmail.com>
2018-02-27 13:47:37 +0100

    Filter out current user from the users list

--- time_manager_client/app/controllers/users.js
+++ time_manager_client/app/controllers/users.js
@@ -1,6 +1,13 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+  currentUser: service(),
+
+  filteredUsers: Ember.computed.filter('model', function(user, index, array) {
+    return this.get('currentUser.user.id') !== user.get('id');
+  }),
+
   actions: {
     deleteUser(user) {
       if (window.confirm('Are you sure, you want to delete this user?')) {
