b75cd079d8bffcdb3f20e83837e8915c744c3507
Peter Toth <tothpeter08@gmail.com>
2018-02-28 21:27:52 +0100

    Use date range picker for tasks filtering

--- time_manager_client/app/controllers/me/tasks.js
+++ time_manager_client/app/controllers/me/tasks.js
@@ -11,22 +11,12 @@ export default Controller.extend({
     this._setDefaultDates();
   },
 
+  dateLimit: moment.duration(3, 'months'),
+
   actions: {
-    changeStartDate(selectedDate) {
-      this.set('startDate', moment(selectedDate).format('YYYY-MM-DD'));
-    },
-
-    changeEndDate(selectedDate) {
-      this.set('endDate', moment(selectedDate).format('YYYY-MM-DD'));
-    },
-
-    filterForCurrentWeek() {
-      this.set('startDate', moment().startOf('week').format('YYYY-MM-DD'));
-      this.set('endDate', moment().endOf('week').format('YYYY-MM-DD'));
-    },
-
-    filterForCurrentMonth() {
-      this._setDefaultDates();
+    setDateRange(newStartDate, newEndDate) {
+      this.set('startDate', newStartDate);
+      this.set('endDate', newEndDate);
     }
   },
 
