1b9437ff229db919c6b5c695d54f4720ee3b5beb
Peter Toth <tothpeter08@gmail.com>
2018-02-28 03:39:14 +0100

    Fix and refactor grouping tasks

--- time_manager_client/app/components/task-list.js
+++ time_manager_client/app/components/task-list.js
@@ -5,32 +5,31 @@ export default Component.extend({
   classNames: ['task-list'],
   preferredWorkingHoursPerDay: 40,
 
-  groupedTasks: computed('tasks', function() {
-    let result = [],
-        currentIndex = -1,
-        currentDate = null;
+  groups: computed('tasks', function() {
+    let groups = [],
+        currentGroup,
+        currentDate;
 
     this.get('tasks').forEach((task) => {
-      if (!currentDate || currentDate.valueOf() != task.get('date').valueOf()) {
-        if (currentDate) {
-          result[currentIndex].underPreferredWorkingHours = result[currentIndex].totalDuration < this.preferredWorkingHoursPerDay;
-        }
-
-        currentIndex++;
+      if (!currentDate || currentDate.valueOf() !== task.get('date').valueOf()) {
         currentDate = task.get('date');
 
-        result.pushObject({
-          date: task.get('date'),
+        currentGroup = groups.pushObject({
+          date: currentDate,
           underPreferredWorkingHours: true,
           totalDuration: 0,
           tasks: []
         });
       }
 
-      result[currentIndex].totalDuration += task.get('duration');
-      result[currentIndex].tasks.pushObject(task);
-     });
+      currentGroup.totalDuration += task.get('duration');
+      currentGroup.tasks.pushObject(task);
+    });
 
-     return result;
+    groups.forEach((group) => {
+      group.underPreferredWorkingHours = group.totalDuration < this.preferredWorkingHoursPerDay;
+    });
+
+    return groups;
   })
 });
