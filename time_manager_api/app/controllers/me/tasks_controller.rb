77802e3dafe4abf027c46268c5b760a901e7a03c
Peter Toth <tothpeter08@gmail.com>
2018-02-28 15:26:54 +0100

    Fix fate filter param check in the backend

--- time_manager_api/app/controllers/me/tasks_controller.rb
+++ time_manager_api/app/controllers/me/tasks_controller.rb
@@ -2,7 +2,7 @@ class Me::TasksController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    if !params[:start_date] || !params[:end_date]
+    if params[:start_date].blank? || params[:end_date].blank?
       return render json: { errors: ['start_date or end_date is missing'] }, status: 422
     end
 
