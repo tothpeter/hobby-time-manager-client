41b88f677b9efc4a09aaa4cc307c00ad7d0177e2
Peter Toth <tothpeter08@gmail.com>
2018-03-02 16:19:27 +0100

    Add backend validation for duration of tasks

--- time_manager_api/app/models/task.rb
+++ time_manager_api/app/models/task.rb
@@ -3,5 +3,7 @@ class Task < ApplicationRecord
 
   validates_presence_of :user, :title, :duration, :date
 
+  validates :duration, numericality: { greater_than: 0, less_than_or_equal_to: 24 * 60 }
+
   scope :between_dates, lambda { |start_date, end_date| where("date BETWEEN ? AND ?", start_date, end_date) }
 end
--- time_manager_api/spec/models/task_spec.rb
+++ time_manager_api/spec/models/task_spec.rb
@@ -8,8 +8,11 @@ describe Task, type: :model do
   describe 'Validations' do
     it { is_expected.to validate_presence_of(:user) }
     it { is_expected.to validate_presence_of(:title) }
-    it { is_expected.to validate_presence_of(:duration) }
     it { is_expected.to validate_presence_of(:date) }
+    it { is_expected.to validate_presence_of(:duration) }
+
+    it { is_expected.to validate_numericality_of(:duration).is_greater_than(0) }
+    it { is_expected.to validate_numericality_of(:duration).is_less_than_or_equal_to(24 * 60) }
   end
 
   describe 'Associations' do
