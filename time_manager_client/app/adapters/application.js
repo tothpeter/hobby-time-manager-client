06815b1431bac3b86408eac58ee07fc18da678d7
Peter Toth <tothpeter08@gmail.com>
2018-03-02 14:37:50 +0100

    Move url name-spacing code to the applicatio ndapter

--- time_manager_client/app/adapters/application.js
+++ time_manager_client/app/adapters/application.js
@@ -2,5 +2,42 @@ import DS from 'ember-data';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
-  authorizer: 'authorizer:application'
+  authorizer: 'authorizer:application',
+
+  urlForQuery(query) {
+    if (query.own) {
+      delete query.own;
+
+      return `/me${this._super(...arguments)}`;
+    }
+
+    return this._super(...arguments);
+  },
+
+  urlForFindRecord(id, modelName, snapshot) {
+    if (snapshot.adapterOptions.me) {
+      delete snapshot.adapterOptions.me;
+      return `/me${this._super(...arguments)}`;
+    }
+
+    return this._super(...arguments);
+  },
+
+  urlForCreateRecord(modelName, snapshot) {
+    if (snapshot.adapterOptions.me) {
+      delete snapshot.adapterOptions.me;
+      return `/me${this._super(...arguments)}`;
+    }
+
+    return this._super(...arguments);
+  },
+
+  urlForUpdateRecord(_id, _modelName, snapshot) {
+    if (snapshot.adapterOptions.me) {
+      delete snapshot.adapterOptions.me;
+      return `/me${this._super(...arguments)}`;
+    }
+
+    return this._super(...arguments);
+  }
 });
