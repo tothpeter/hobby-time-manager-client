fac4bb4ab4af593fde4a12c0fe041a62cd47a1ce
Peter Toth <tothpeter08@gmail.com>
2018-03-02 16:25:33 +0100

    Add missing test

--- time_manager_api/spec/models/authentication_token_spec.rb
+++ time_manager_api/spec/models/authentication_token_spec.rb
@@ -1,5 +1,5 @@
-require 'rails_helper'
-
 describe AuthenticationToken, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Associations' do
+    it { is_expected.to belong_to(:user) }
+  end
 end
