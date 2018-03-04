5b82666474adbdd457e235a6b25eeaae60cf7264
Peter Toth <tothpeter08@gmail.com>
2018-03-04 01:41:13 +0100

    Refactor: it is enough to authenticate the root dashboard route

--- time_manager_client/app/routes/dashboard/users/user/tasks/index.js
+++ time_manager_client/app/routes/dashboard/users/user/tasks/index.js
@@ -1,8 +1,7 @@
 import Route from '@ember/routing/route';
-import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 import RSVP from 'rsvp';
 
-export default Route.extend(AuthenticatedRouteMixin, {
+export default Route.extend({
   queryParams: {
     dateRange: {
       refreshModel: true
--- time_manager_client/app/routes/dashboard/users/user/tasks/new.js
+++ time_manager_client/app/routes/dashboard/users/user/tasks/new.js
@@ -1,7 +1,6 @@
 import Route from '@ember/routing/route';
-import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Route.extend(AuthenticatedRouteMixin, {
+export default Route.extend({
   model(params) {
     let user = this.modelFor('dashboard.users.user'),
         task = this.store.createRecord('task', { user: user });
