dff23fbc10511ff8bc6ee4a94f4385dae92cbe38
Peter Toth <tothpeter08@gmail.com>
2018-03-04 01:49:03 +0100

    Remove unused param

--- time_manager_client/app/routes/dashboard/users/user/tasks/new.js
+++ time_manager_client/app/routes/dashboard/users/user/tasks/new.js
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  model(params) {
+  model() {
     let user = this.modelFor('dashboard.users.user'),
         task = this.store.createRecord('task', { user: user });
 
