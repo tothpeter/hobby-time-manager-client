ca79d0ea3056b4671952695a51943fb6f04fab7c
Peter Toth <tothpeter08@gmail.com>
2018-03-05 16:06:57 +0100

    Cover /me/tasks create and update methods with tests

--- time_manager_api/spec/requests/me/tasks_spec.rb
+++ time_manager_api/spec/requests/me/tasks_spec.rb
@@ -105,4 +105,131 @@ describe '/me/tasks' do
       expect(response.code).to eq('401')
     end
   end
+
+  describe "POST #create" do
+    context "when the current user creates a task for himsel" do
+      it "creates a new task and returns it" do
+        request_arguments = {
+          headers: auth_headers,
+          params: create_request_payload(current_user),
+          as: :json
+        }
+
+        post '/me/tasks', request_arguments
+
+        expect(json_response[:data][:attributes][:title]).to eq('Test title')
+        expect(Task.count).to eq(1)
+        expect(Task.last.user_id).to eq(current_user.id)
+        expect(response).to have_http_status(:created)
+      end
+    end
+
+    context "when the current user creates a task for someone else" do
+      it "creates a new task for himsel anyway and returns it" do
+        other_user = FactoryBot.create :user
+
+        request_arguments = {
+          headers: auth_headers,
+          params: create_request_payload(other_user),
+          as: :json
+        }
+
+        post '/me/tasks', request_arguments
+
+        expect(json_response[:data][:attributes][:title]).to eq('Test title')
+        expect(Task.count).to eq(1)
+        expect(Task.last.user_id).to eq(current_user.id)
+        expect(response).to have_http_status(:created)
+      end
+    end
+  end
+
+
+  describe "PATCH #update" do
+    context "when the current user updates his own task" do
+      it "updates the task and returns it" do
+        task = FactoryBot.create :task, user: current_user
+        request_payload  = patch_request_payload task, current_user
+        updated_title = request_payload[:data][:attributes][:title] += 'Update1'
+
+        request_arguments = {
+          headers: auth_headers,
+          params: request_payload,
+          as: :json
+        }
+
+        patch "/me/tasks/#{task.id}", request_arguments
+
+        expect(response.code).to eq('200')
+        expect(json_response[:data][:attributes][:title]).to eq(updated_title)
+      end
+    end
+
+    context "when the current user assigns his task to someone else" do
+      it "won't assign the task to someone else" do
+        other_user = FactoryBot.create :user
+        task = FactoryBot.create :task, user: current_user
+        request_payload = patch_request_payload task, other_user
+        updated_title = request_payload[:data][:attributes][:title] += 'Update1'
+
+        request_arguments = {
+          headers: auth_headers,
+          params: request_payload,
+          as: :json
+        }
+
+        patch "/me/tasks/#{task.id}", request_arguments
+
+        expect(response.code).to eq('200')
+        expect(json_response[:data][:attributes][:title]).to eq(updated_title)
+        expect(Task.last.user_id).to eq(current_user.id)
+      end
+    end
+  end
+
+  def patch_request_payload task, owner
+    {
+      data: {
+        id: task.id,
+        attributes: {
+          title: task.title,
+          description: task.description,
+          date: "#{task.date}T00:00:00.000Z",
+          duration: task.duration
+        },
+        relationships: {
+          user: {
+            data: {
+              type: 'users',
+              id: owner.id
+            }
+          }
+        },
+
+        type: 'tasks'
+      }
+    }
+  end
+
+  def create_request_payload user
+    {
+      data:{
+        attributes: {
+          title: "Test title",
+          description: "Test desc",
+          date: "2018-03-05T13:39:48.758Z",
+          duration: 1
+        },
+        relationships: {
+          user: {
+            data: {
+              type: 'users',
+              id: user.id
+            }
+          }
+        },
+        type: 'tasks'
+      }
+    }
+  end
 end
