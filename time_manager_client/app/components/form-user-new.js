dcaef9fe43d620a5539f8d9d532fedc35bcefe98
Peter Toth <tothpeter08@gmail.com>
2018-03-06 00:13:08 +0100

    Add access level select to new user form

--- time_manager_client/app/components/form-user-new.js
+++ time_manager_client/app/components/form-user-new.js
@@ -1,8 +1,10 @@
 import Component from '@ember/component';
 import { w } from '@ember/string';
 import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
+  currentUser: service(),
   submitButtonLabel: 'Sign up',
 
   externalErrors: computed('model.errors.[]', function() {
