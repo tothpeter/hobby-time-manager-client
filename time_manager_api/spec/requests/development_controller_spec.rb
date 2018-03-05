bef34bccceffdf4b831aba1121e5fa4802ec6f8e
Peter Toth <tothpeter08@gmail.com>
2018-03-05 13:35:30 +0100

    Convert /development controller test to request test

--- time_manager_api/spec/requests/development_controller_spec.rb
+++ time_manager_api/spec/requests/development_controller_spec.rb
@@ -1,8 +1,8 @@
-describe DevelopmentController, type: :controller do
+describe '/development' do
 
   describe "GET #tasks_export" do
     it "returns 404 when we are not in development" do
-      get :tasks_export
+      get '/development/tasks_export'
       expect(response).to have_http_status(404)
     end
   end
