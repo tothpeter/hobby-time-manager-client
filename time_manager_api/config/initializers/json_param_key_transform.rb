05198c99f7396acda32409490cd280a7cc67399b
Peter Toth <tothpeter08@gmail.com>
2018-03-05 15:35:39 +0100

    Handle request with 'null' body content

--- time_manager_api/config/initializers/json_param_key_transform.rb
+++ time_manager_api/config/initializers/json_param_key_transform.rb
@@ -1,6 +1,8 @@
 # Transform JSON request param keys from JSON-conventional camelCase to
 # Rails-conventional snake_case:
 ActionDispatch::Request.parameter_parsers[:json] = -> (raw_post) {
+  return {} if raw_post == 'null'
+
   # Modified from action_dispatch/http/parameters.rb
   data = ActiveSupport::JSON.decode(raw_post)
   data = {:_json => data} unless data.is_a?(Hash)
