777f49147a6e1544824e6ccc1401d5a6739fdfad
Peter Toth <tothpeter08@gmail.com>
2018-03-05 13:33:28 +0100

    Convert /me/tasks controller test to a request test

--- time_manager_api/spec/requests/me/tasks_spec.rb
+++ time_manager_api/spec/requests/me/tasks_spec.rb
@@ -1,14 +1,14 @@
-describe Me::TasksController, type: :controller do
+describe '/me/tasks' do
 
   describe "#validate_date_filter" do
-    before do
-      current_user = FactoryBot.create :user
-      authenticate_request current_user
-    end
-
     context "when the start date filter is missing" do
       it "returns a 422" do
-        get :index, params: { start_date: 1 }
+        request_arguments = {
+          headers: auth_headers,
+          params: { start_date: 1 }
+        }
+
+        get '/me/tasks', request_arguments
 
         expect(json_response[:errors][0][:title]).to include('missing')
         expect(response.code).to eq('422')
@@ -17,7 +17,12 @@ describe Me::TasksController, type: :controller do
 
     context "when the end date filter is missing" do
       it "returns a 422" do
-        get :index, params: { end_date: 1 }
+        request_arguments = {
+          headers: auth_headers,
+          params: { end_date: 1 }
+        }
+
+        get '/me/tasks', request_arguments
 
         expect(json_response[:errors][0][:title]).to include('missing')
         expect(response.code).to eq('422')
@@ -26,7 +31,15 @@ describe Me::TasksController, type: :controller do
 
     context "when the time gap between start and end date is bigger than 3 months" do
       it "returns a 422" do
-        get :index, params: { start_date: '2000-12-01', end_date: '2001-03-04' }
+        request_arguments = {
+          headers: auth_headers,
+          params: {
+            start_date: '2000-12-01',
+            end_date: '2001-03-04'
+          }
+        }
+
+        get '/me/tasks', request_arguments
 
         expect(json_response[:errors][0][:title]).to include('months')
         expect(response.code).to eq('422')
@@ -35,7 +48,15 @@ describe Me::TasksController, type: :controller do
 
     context "when the params contain something funny" do
       it "returns a 422" do
-        get :index, params: { start_date: 'hack', end_date: 'malicious content' }
+        request_arguments = {
+          headers: auth_headers,
+          params: {
+            start_date: 'hack',
+            end_date: 'malicious content'
+          }
+        }
+
+        get '/me/tasks', request_arguments
 
         expect(json_response[:errors][0][:title]).to include('invalid')
         expect(response.code).to eq('422')
@@ -44,7 +65,15 @@ describe Me::TasksController, type: :controller do
 
     context "when the params are good" do
       it "returns a 422" do
-        get :index, params: { start_date: '2000-01-01', end_date: '2000-04-01' }
+        request_arguments = {
+          headers: auth_headers,
+          params: {
+            start_date: '2000-01-01',
+            end_date: '2000-04-01'
+          }
+        }
+
+        get '/me/tasks', request_arguments
 
         expect(response.code).to eq('200')
       end
@@ -53,24 +82,25 @@ describe Me::TasksController, type: :controller do
 
   describe "GET #index" do
     it "returns the tasks of the current user only" do
-      current_user = FactoryBot.create :user
       FactoryBot.create :task
       task = FactoryBot.create :task, user: current_user
-      authenticate_request current_user
 
-      params = {
-        start_date: task.date,
-        end_date: task.date
+      request_arguments = {
+        headers: auth_headers,
+        params: {
+          start_date: task.date,
+          end_date: task.date
+        }
       }
 
-      get :index, params: params
+      get '/me/tasks', request_arguments
 
       expect(json_response[:data][0][:id].to_i).to eq(task.id)
       expect(json_response[:data].length).to eq(1)
     end
 
     it "returns a 401 when the user is not authenticated" do
-      get :index
+      get '/me/tasks'
 
       expect(response.code).to eq('401')
     end
