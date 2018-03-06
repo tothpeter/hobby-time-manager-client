a9c6e1ac95efd653ff1b07d4051527515b1e83ec
Peter Toth <tothpeter08@gmail.com>
2018-03-06 17:12:41 +0100

    Add comment for refactoring

--- time_manager_client/app/services/ajax.js
+++ time_manager_client/app/services/ajax.js
@@ -7,6 +7,19 @@ export default AjaxService.extend({
   session: service(),
   host: config.apiHost,
 
+  // TODO: refactor: remove header settings from request, the code below generates some wierd errors in a completely different part of the app
+  // headers: computed('session.data.authenticated.{token, email}', {
+  //   get() {
+  //     let headers = {};
+  //
+  //     this.get('session').authorize('authorizer:application', (headerName, headerValue) => {
+  //       headers[headerName] = headerValue;
+  //     });
+  //
+  //     return headers;
+  //   }
+  // }),
+
   request(url, options) {
     this.get('session').authorize('authorizer:application', (headerName, headerValue) => {
       this.set(`headers.${headerName}`, headerValue);
