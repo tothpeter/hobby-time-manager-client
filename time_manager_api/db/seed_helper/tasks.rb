3cd91f58dffa42ed4929d275478b8b505b788e04
Peter Toth <tothpeter08@gmail.com>
2018-03-07 16:56:56 +0100

    Update seed

--- time_manager_api/db/seed_helper/tasks.rb
+++ time_manager_api/db/seed_helper/tasks.rb
@@ -14,6 +14,26 @@ module SeedHelper::Tasks
 
   def self.tasks_params_for_me
     result = []
+
+    2.times do |i|
+      result << {
+        user_id: 1,
+        title: "Task with long desc",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        date: Date.yesterday,
+        duration: 10 # minutes
+      }
+    end
+
+
+    result << {
+      user_id: 1,
+      title: "Task with line breaks in desc",
+      description: "Lorem ipsum dolor sit amet,\n\rconsectetur adipisicing elit, sed do\n\r\n\reiusmod",
+      date: Date.yesterday,
+      duration: 10 # minutes
+    }
+
     3.times do |i|
       result << {
         user_id: 1,
